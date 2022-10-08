a1dac1a9577debb165e72b78890ba749003a49c3
AllanEl <allanrnelias@gmail.com>
2022-10-08 04:40:46 -0400

    added rows and columns

---
+++ Develop/main.js
@@ -0,0 +1,19 @@
+
+// Today's date
+var $currentDay = $('#currentDay');
+var todaysdate = moment();
+$currentDay.text(todaysdate.format("dddd, MMMM Do"));
+
+
+ 
+
+var textarea = $('textarea');
+
+var currentTimeHr = moment().hour();
+
+
+
+// var segmentHour = 
+
+// if(moment)
+// $("textarea").attr("class", "present")
