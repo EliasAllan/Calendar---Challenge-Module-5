c71cc890383b4a30bc53b45db64a57dbc9e25d83
AllanEl <allanrnelias@gmail.com>
2022-10-10 02:42:44 -0400

    appended elements to page

--- Develop/main.js
+++ Develop/main.js
@@ -1,3 +1,17 @@
+// Create
+
+var $9AMblock = $("#9");
+var $10AMblock = $("#10");
+var $11AMblock = $("#11");
+var $12AMblock = $("#12");
+var $1PMblock = $("#1");
+var $2PMblock = $("#2");
+var $3PMblock = $("#3");
+var $4PMblock = $("#4");
+var $5PMblock = $("#5");
+
+// Append
+$9AMblock.append(CheckTime)
 
 // Today's date
 var $currentDay = $('#currentDay');
@@ -5,21 +19,26 @@ var todaysdate = moment();
 $currentDay.text(todaysdate.format("dddd, MMMM Do"));
 
 
- 
+// Time Variables
 
-var textarea = $('textarea');
-
+// var textarea = $('textarea');
 var currentTimeHr = moment().hour();
 
-segmentTime = "";
+var segmentTime = parseInt(document.body.children[1].children[0].children[1].id);
 
-function checkhour(){
-var segmentTime = parseInt(document.getElementsByTagName("textarea")[0])
+
+function CheckTime(){
+if(currentTimeHr === segmentTime){
+    $9AMblock.addClass("present")
+    
+}else if(currentTimeHr < segmentTime){
+    $9AMblock.addClass("future")
+    
+}else if(currentTimeHr > segmentTime){
+    $9AMblock.addClass("past")
+    
 }
-
-// if(currentTimeHr === segmentTime){
-//     textarea.attr("class", "present")
-// }
+}
 // var segmentHour = 
 
 // if(moment)
