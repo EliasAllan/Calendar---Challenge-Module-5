cf786b86998efa3188672b44b3ece6b54c217526
AllanEl <allanrnelias@gmail.com>
2022-10-10 18:31:18 -0400

    added functions to add the proper color background to the text areas

--- Develop/main.js
+++ Develop/main.js
@@ -1,5 +1,7 @@
 // Create
 
+var textAreas = document.getElementsByTagName('textarea')
+var buttons = document.getElementsByTagName('button')
 var $9AMblock = $("#9");
 var $10AMblock = $("#10");
 var $11AMblock = $("#11");
@@ -9,34 +11,68 @@ var $2PMblock = $("#2");
 var $3PMblock = $("#3");
 var $4PMblock = $("#4");
 var $5PMblock = $("#5");
-
 // Append
-$9AMblock.append(CheckTime)
 
 // Today's date
 var $currentDay = $('#currentDay');
 var todaysdate = moment();
 $currentDay.text(todaysdate.format("dddd, MMMM Do"));
 
+// buttons[0].addEventListener('click', example)
 
+// function example(event){
+//     event.preventDefault()
+//     console.log(event.target.previousSibling.value)
+// }
 // Time Variables
 
 // var textarea = $('textarea');
-var currentTimeHr = moment().hour();
-var segmentTime = parseInt(document.body.children[1].children[0].children[1].id);
 
+// var segmentTime = parseInt(document.body.children[7].children[0].children[1].id);
+// console.log(currentTimeHr)
+// console.log(document.body.children[7].children[0].children[1].id)
 
-function CheckTime(){
-if(currentTimeHr < segmentTime){
-    $9AMblock.addClass("past");
-    return;
-}else if(currentTimeHr > segmentTime){
-    $9AMblock.addClass("future");
-    return; 
+function checkTime(segment,segmentTime){
+    var currentTimeHr = moment().hour();
+    console.log(segment)
+    // console.log(currentTimeHr)
+    // console.log(segmentTime)
+if(segmentTime < currentTimeHr){
+    segment.classList.add("past");
+}else if(segmentTime > currentTimeHr){
+    segment.classList.add("future");
 }else{
-    $9AMblock.addClass("present");
-    return;
+    segment.classList.add("present");
 }
+}
+
+function addClasses(){
+for(let i = 0; i<textAreas.length; i++){
+    // console.log(textAreas[i].id)
+    checkTime(textAreas[i], textAreas[i].id)
+    console.log(localStorage.getItem(textAreas[i]))
+   textAreas[i].innerHTML = localStorage.getItem(textAreas[i].id)
+}
+}
+
+addClasses()
+
+function addEventListenersToSave(){
+    for( i = 0 ; i < buttons.length ; i++){
+        console.log(buttons[i])
+        buttons[i].addEventListener("click", savetoLocalStorage)
+    }
+   //iterate through each button
+   //add event listener that on click calls fucntion "savetoLocalStorage"
+}
+
+addEventListenersToSave()
+
+function savetoLocalStorage(event){
+    //it will event.stopPropagation()
+    event.stopPropagation()
+    //you will find the text area next to the button via event.target
+    //travel to my textArea, and save to local storage what is in that text area using the textARea id as the key
 }
 // var segmentHour = 
 
