298c0cfb3a97e31d6a7977d6b7ea725bbddf3b16
AllanEl <allanrnelias@gmail.com>
2022-10-11 14:56:23 -0400

    fixed local storage rendering

--- Develop/main.js
+++ Develop/main.js
@@ -1,7 +1,7 @@
 // Create
 
-var textAreas = document.getElementsByTagName('textarea')
-var buttons = document.getElementsByTagName('button')
+var textAreas = document.getElementsByTagName("textarea");
+var buttons = document.getElementsByTagName("button");
 // var $9AMblock = $("#9");
 // var $10AMblock = $("#10");
 // var $11AMblock = $("#11");
@@ -15,9 +15,11 @@ var buttons = document.getElementsByTagName('button')
 // Append
 
 // Today's date
-var $currentDay = $('#currentDay');
+var $currentDay = $("#currentDay");
 var todaysdate = moment();
 $currentDay.text(todaysdate.format("dddd, MMMM Do"));
+var blocks = JSON.parse(localStorage.getItem('blocks')) || [null,null,null,null,null,null,null,null,null]
+// console.log(blocks)
 
 // buttons[0].addEventListener('click', example)
 
@@ -26,60 +28,60 @@ $currentDay.text(todaysdate.format("dddd, MMMM Do"));
 //     console.log(event.target.previousSibling.value)
 // }
 
-
-function checkTime(segment,segmentTime){
-    var currentTimeHr = moment().hour();
-    console.log(segment)
-    // console.log(currentTimeHr)
-    // console.log(segmentTime)
-if(segmentTime < currentTimeHr){
+function checkTime(segment, segmentTime) {
+  var currentTimeHr = moment().hour();
+  //console.log(segment);
+  // console.log(currentTimeHr)
+  // console.log(segmentTime)
+  if (segmentTime < currentTimeHr) {
     segment.classList.add("past");
-}else if(segmentTime > currentTimeHr){
+  } else if (segmentTime > currentTimeHr) {
     segment.classList.add("future");
-}else{
+  } else {
     segment.classList.add("present");
-}
+  }
 }
 
-function addClasses(){
-for(let i = 0; i<textAreas.length; i++){
+function addClasses() {
+  for (let i = 0; i < textAreas.length; i++) {
     // console.log(textAreas[i].id)
-    checkTime(textAreas[i], textAreas[i].id)
+    checkTime(textAreas[i], textAreas[i].id);
     // console.log(localStorage.getItem(textAreas[i]))
-   textAreas[i].innerHTML = localStorage.getItem("blocks")
-}
-}
-
-addClasses()
-
-
-
-function addEventListenersToSave(){
-    for( i = 0 ; i < buttons.length ; i++){
-        console.log(buttons[i])
-        buttons[i].addEventListener("click", savetoLocalStorage)
-    }
-   //iterate through each button
-   //add event listener that on click calls fucntion "savetoLocalStorage"
-}
-
-addEventListenersToSave()
-
-
-var blocks = [];
-
-function savetoLocalStorage(){
-    // it will event.stopPropagation()
    
-    for(let i = 0; i < textAreas.length; i++){
-        
-        localStorage.setItem("blocks", JSON.stringify($("#9").val()));
-        // localStorage.setItem("blocks", JSON.stringify($("#10").val()));
-        // localStorage.push("blocks", JSON.stringify($("#11").val()));
-        // localStorage.push("blocks", JSON.stringify($("#12").val()));
-        console.log(JSON.stringify($("#9").val()))
-    }
-    //you will find the text area next to the button via event.target
-    //travel to my textArea, and save to local storage what is in that text area using the textArea id as the key
+    textAreas[i].innerHTML = blocks[i];
+  }
 }
 
+
+function addEventListenersToSave() {
+  for (i = 0; i < buttons.length; i++) {
+    //console.log(buttons[i]);
+    buttons[i].addEventListener("click", savetoLocalStorage);
+  }
+  //iterate through each button
+  //add event listener that on click calls fucntion "savetoLocalStorage"
+}
+
+addEventListenersToSave();
+
+function renderStorage(){
+  blocks = JSON.parse(localStorage.getItem('blocks'))
+  console.log(blocks)
+  addClasses()  
+}
+
+function savetoLocalStorage(event) {
+  // it will event.stopPropagation()
+// console.log(buttons[i])
+  for (let i = 0; i < buttons.length; i++) {
+if(event.target !== buttons[i]){
+  continue  
+}
+blocks[i] = textAreas[i].value
+localStorage.setItem('blocks', JSON.stringify(blocks))
+renderStorage()
+  }
+  //you will find the text area next to the button via event.target
+  //travel to my textArea, and save to local storage what is in that text area using the textArea id as the key
+}
+addClasses();
