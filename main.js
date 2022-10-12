d526756a85e7cbdfb44b4865d8f4cf72202534c1
AllanEl <allanrnelias@gmail.com>
2022-10-12 18:41:17 -0400

    quickfix

--- main.js
+++ main.js
@@ -1,38 +1,17 @@
-// Create
-
 var textAreas = document.getElementsByTagName("textarea");
 var buttons = document.getElementsByTagName("button");
-// var $9AMblock = $("#9");
-// var $10AMblock = $("#10");
-// var $11AMblock = $("#11");
-// var $12AMblock = $("#12");
-// var $1PMblock = $("#1");
-// var $2PMblock = $("#2");
-// var $3PMblock = $("#3");
-// var $4PMblock = $("#4");
-// var $5PMblock = $("#5");
 
-// Append
+
 
 // Today's date
 var $currentDay = $("#currentDay");
 var todaysdate = moment();
 $currentDay.text(todaysdate.format("dddd, MMMM Do"));
 var blocks = JSON.parse(localStorage.getItem('blocks')) || [null,null,null,null,null,null,null,null,null]
-// console.log(blocks)
 
-// buttons[0].addEventListener('click', example)
-
-// function example(event){
-//     event.preventDefault()
-//     console.log(event.target.previousSibling.value)
-// }
 
 function checkTime(segment, segmentTime) {
   var currentTimeHr = moment().hour();
-  //console.log(segment);
-  // console.log(currentTimeHr)
-  // console.log(segmentTime)
   if (segmentTime < currentTimeHr) {
     segment.classList.add("past");
   } else if (segmentTime > currentTimeHr) {
@@ -44,10 +23,7 @@ function checkTime(segment, segmentTime) {
 
 function addClasses() {
   for (let i = 0; i < textAreas.length; i++) {
-    // console.log(textAreas[i].id)
     checkTime(textAreas[i], textAreas[i].id);
-    // console.log(localStorage.getItem(textAreas[i]))
-   
     textAreas[i].innerHTML = blocks[i];
   }
 }
@@ -55,11 +31,8 @@ function addClasses() {
 
 function addEventListenersToSave() {
   for (i = 0; i < buttons.length; i++) {
-    //console.log(buttons[i]);
     buttons[i].addEventListener("click", savetoLocalStorage);
   }
-  //iterate through each button
-  //add event listener that on click calls fucntion "savetoLocalStorage"
 }
 
 addEventListenersToSave();
@@ -71,8 +44,6 @@ function renderStorage(){
 }
 
 function savetoLocalStorage(event) {
-  // it will event.stopPropagation()
-// console.log(buttons[i])
   for (let i = 0; i < buttons.length; i++) {
 if(event.target !== buttons[i]){
   continue  
@@ -81,8 +52,6 @@ blocks[i] = textAreas[i].value
 localStorage.setItem('blocks', JSON.stringify(blocks))
 renderStorage()
   }
-  //you will find the text area next to the button via event.target
-  //travel to my textArea, and save to local storage what is in that text area using the textArea id as the key
 }
 addClasses();
 
